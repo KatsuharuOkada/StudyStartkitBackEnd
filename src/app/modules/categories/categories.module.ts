c3a1a2a98ef12a8d7c29458331dca98e828e4f24
Giang Nguyen <40553220+vfa-giangnt@users.noreply.github.com>
2023-11-16 15:09:47 +0700

    Initialize feature/prettierc_use_gts

---
+++ src/app/modules/categories/categories.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryRepository } from './category.repository';
+import { CategoriesResolver } from './categories.resolver';
+import { CategoriesService } from './categoies.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([CategoryRepository])],
+  providers: [CategoriesResolver, CategoriesService],
+  exports: [],
+})
+export class CategoriesModule {}
---
+++ src/app/modules/users/users.resolver.spec.ts
@@ -0,0 +1,56 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+import { User } from '../../entities/user.entity';
+
+describe('CategoriesResolver', () => {
+  let resolver: UsersResolver;
+  let service: UsersService;
+  const mockServiceFun = {
+    getUserInfoByUserId: async (userId: number): Promise<Partial<User>> => {
+      return userId === 1
+        ? {
+            email: 'test@gmail.com',
+            fullname: 'full Name',
+            gender: 'MALE',
+            userId: 1,
+          }
+        : null;
+    },
+  };
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: UsersService,
+          useFactory: jest.fn(() => ({
+            createUser: async () => 1,
+            getUserInfoByUserId: jest.fn((userId) =>
+              mockServiceFun.getUserInfoByUserId(userId)
+            ),
+          })),
+        },
+        UsersResolver,
+      ],
+    }).compile();
+
+    resolver = module.get<UsersResolver>(UsersResolver);
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+  it('should be defined', async () => {
+    const result = await resolver.getUserInformation(1);
+    expect(result).toMatchObject({
+      data: {
+        email: 'test@gmail.com',
+        fullname: 'full Name',
+        gender: 'MALE',
+        userId: 1,
+      },
+    });
+  });
+});

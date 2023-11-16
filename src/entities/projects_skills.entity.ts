dbcc0977d69037d6e456461de29e2d3b7fc1c370
Giang Nguyen <40553220+vfa-giangnt@users.noreply.github.com>
2023-11-16 15:09:52 +0700

    Initialize Intership-202308

---
+++ src/entities/projects_skills.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import { AbstractEntity } from '../vendors/base/abstract.entity';
+import { ProjectsEntity } from './projects.entity';
+import { SkillsEntity } from './skills.entity';
+
+@Entity({ name: 'projects_skills' })
+export class ProjectsSkillsEntity extends AbstractEntity {
+  @ManyToOne(() => ProjectsEntity, { onDelete: 'RESTRICT', onUpdate: 'CASCADE', eager: true, nullable: false })
+  @JoinColumn({ name: 'project_id', referencedColumnName: 'id' })
+  // Same table name using camel case
+  projects: ProjectsEntity;
+
+  @ManyToOne(() => SkillsEntity, { onDelete: 'RESTRICT', onUpdate: 'CASCADE', eager: true, nullable: false })
+  @JoinColumn({ name: 'skill_id', referencedColumnName: 'id' })
+  // Same table name using camel case
+  skills: SkillsEntity;
+}
